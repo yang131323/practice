f2defe9c1dae9a6df94ec6e7a40d2816e83f9ff0
is Q <1509525854@qq.com>
2019-04-15 15:39:03 +0800

    添加leetCode，广度搜索刷题记录

--- js/array.js
+++ js/array.js
@@ -1,1 +1,13 @@
-// 使用Proxy模拟实现数组或者使用访问器属性模拟实现数组（286）
+// 使用Proxy模拟实现数组或者使用访问器属性模拟实现数组（286
+
+/**
+ * 多维数组扁平化
+ * @param {*} arr 
+ */
+function flatArray (arr) {
+  return Array.isArray(arr) ? arr.reduce(function (a, b) {
+    let ta = !a ? [] : [...a];
+    let tb = !b ? [] : [...flatArray(b)];
+    return [...ta, ...tb];
+  }, []) : [arr];
+}
