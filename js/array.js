7b2b1d561859130b6adae88471311687599bdd37
is Q <1509525854@qq.com>
2019-05-28 15:02:55 +0800

    添加深拷贝

--- js/array.js
+++ js/array.js
@@ -1,3 +1,5 @@
+import { isArray, isObject, isNull } from "./utils";
+
 // 使用Proxy模拟实现数组或者使用访问器属性模拟实现数组（286）
 // 个人觉得扁平化就是递归，比较喜欢concat和reduce版本
 
@@ -29,4 +31,76 @@ function flatConcat(arr) {
   if (arr.some(item => Array.isArray(item))) {
     return flatConcat([].concat(...arr));
   } else { return arr; }
+}
+
+/**
+ * 将数组随机打乱方式一
+ * 使用数组的sort方法进行随机，不是真正的随机
+ * @param {Array} arr 
+ */
+function randomArrayOne (arr) {
+  if (!isArray(arr)) { throw new Error('argument except is a array!'); }
+  return arr.sort(() => {
+    return .5 - Math.random();
+  });
+}
+
+/**
+ * 将数组随机打乱方式二
+ * 每次确定一个位置的数字-这个数字是随机确定的
+ * @param {*} arr 
+ */
+function randomArrayTwo (arr) {
+  if (!isArray(arr)) { throw new Error('argument except is a array!'); }
+  let index = arr.length;
+  let i, temp;
+  while (index) {
+    i = Math.floor(Math.random() * index--);
+    temp = arr[i];
+    arr[i] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+}
+
+/**
+ * 对数据进行深拷贝，循环引用会导致栈溢出
+ * @param {Object} data // 被拷贝对象 
+ * @param {Object} target // 返回对象
+ */
+function deepCloneOne (data, target) {
+  if (!isArray(data) && !isObject(data)) { return data; }
+  target = target || {};
+  for (let x in data) {
+    target[x] = isArray(data[x]) ? [] : {};
+    target[x] = deepClone(data[x], target[x]);
+  }
+  return target;
+}
+
+/**
+ * 对数据进行深拷贝，对循环引用进行了处理
+ * @param {Object} data 被拷贝对象
+ */
+function deepCloneTwo (data) {
+  if (!isArray(data) && !isObject(data)) { return data; } // 非对象数组已经作为基本值返回
+  let parents = [];
+  let childs = [];
+  const _clone = parent => {
+    if (isNull(parent) || (!isArray(parent) && !isObject(parent))) { return parent; }
+    let child = [];
+    if (isObject(parent)) {
+      const temp = Object.getPrototypeOf(parent);
+      child = Object.create(temp);
+    }
+    const index = parents.indexOf(parent);
+    if (index !== -1) { return childs[index]; }
+    parents.push(parent); // 必须在遍历之前放进去，因为在遍历的时候如果碰到了本次遍历遇到的属性，将无法终止
+    childs.push(child); // 必须在遍历之前放进去，因为在遍历的时候如果碰到了本次遍历遇到的属性，将无法终止
+    for (let x in parent) {
+      child[x] = _clone(parent[x]);
+    }
+    return child;
+  }
+  return _clone(data);
 }
--- js/utils.js
+++ js/utils.js
@@ -1,43 +1,51 @@
-export const isNull = function (val) {
-  return val === val;
+export function isNull  (val) {
+  return val === null;
 }
 
-export const isNaN = function (val) {
+export function isNaN  (val) {
   return val !== val; // Number.isNaN(val)
 }
 
-export const isUndefined = function (val) {
+export function isUndefined  (val) {
   return val === undefined;
 }
 
-export const isFunction = function (val) {
+export function isFunction  (val) {
   return typeof val === 'function';
 }
 
-export const isArray = function (val) {
+export function isArray  (val) {
   return Array.isArray(val);
 }
 
-export const isBoolean = function (val) {
+export function isBoolean  (val) {
   return typeof val === 'boolean';
 }
 
-export const isNumber = function (val) {
+export function isNumber  (val) {
   return typeof val === 'number';
 }
 
-export const isString = function (val) {
+export function isString  (val) {
   return typeof val === 'string';
 }
 
-export const isSymbol = function (val) {
+export function isSymbol  (val) {
   return typeof val === 'symbol';
-}
-
-export const isObject = function (val) {
-  return Object.prototype.toString.call(val) === '[object Object]';
 }
 
 export function isNullUndefinedNaN (val) {
   return isNull(val) || isUndefined(val) || isNaN(val);
+}
+
+export function isObject  (val) {
+  return Object.prototype.toString.call(val) === '[object Object]';
+}
+
+export function isDate (val) {
+  return Object.prototype.toString.call(val) === '[object Date]';
+}
+
+export function isRegEXp () {
+  return Object.prototype.toString.call(val) === '[object RegExp]';
 }
